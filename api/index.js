b1c33103fe0ede3daf42bc0d120dde6abc799a7f
Piyush_Tomar <piyushtomarp@gmail.com>
2026-05-04 01:20:21 +0530

    debug: expose error in api response + fix includeFiles to server/src only

--- api/index.js
+++ api/index.js
@@ -1,13 +1,28 @@
 require('dotenv').config();
-const connectDB = require('../server/src/config/db');
-const app = require('../server/src/app');
 
+let app;
 let isConnected = false;
 
 module.exports = async (req, res) => {
-  if (!isConnected) {
-    await connectDB();
-    isConnected = true;
+  try {
+    // Lazy-load to catch module errors
+    if (!app) {
+      const connectDB = require('../server/src/config/db');
+      app = require('../server/src/app');
+
+      if (!isConnected) {
+        await connectDB();
+        isConnected = true;
+      }
+    }
+
+    return app(req, res);
+  } catch (error) {
+    console.error('[Serverless] Fatal error:', error);
+    return res.status(500).json({
+      message: 'Internal Server Error',
+      error: error.message,
+      stack: error.stack
+    });
   }
-  return app(req, res);
 };
